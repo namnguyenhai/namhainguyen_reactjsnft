4e40b3a17891c40692fa39736ced3ebdecf0a11a
namnguyenhai <nguyenhainam26n@gmail.com>
2023-05-24 00:17:53 +0700

    code cart

--- reactjsnft/src/App.js
+++ reactjsnft/src/App.js
@@ -1,16 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Button } from './Component/Button';
-
+import { Cart } from 'Component/Card';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button percent={10} onClick={() => console.log('After click')} > click me</Button>
+        {/* <Button percent={10} onClick={() => console.log('After click')} > click me</Button>
         <Button percent={12.3}>12.3%</Button>
         <Button percent={8.1}>8.1%</Button>
         <Button percent={- 5.1}>5.1%</Button>
-        <Button percent={3.2}>3.2%</Button>
+        <Button percent={3.2}>3.2%</Button> */}
+        <Cart tittle={"Revenue"} amount={"5.00"} percent={12.3}></Cart>
       </header>
     </div>
   );
--- reactjsnft/src/Component/Card/Card.jsx
+++ reactjsnft/src/Component/Card/Card.jsx
@@ -1,3 +1,38 @@
-export const Cart = () => {
-
+import styled from "styled-components";
+import { Button } from "Component/Button";
+import Ether from "images/Ether.svg";
+const StyledCard = styled.div`
+    padding: 24px;
+    width: 164px;
+    height: 167px;
+    border: 1px solid;
+    background-color: #FFFFFF;
+    border-radius: 12px;
+    color: #747475;
+    .tittle{
+        color: #747475;
+        font-weight: 700;
+        font-size: 14px;
+        text-align: left;
+        margin-bottom: 24px;
+    }
+    .amount{
+        font-size: 24px;
+        color: #27262E;
+    }
+    .ammount-wrapper {
+        margin-bottom: 24px;
+    }
+`
+export const Cart = ({tittle,amount,content,percent}) => {
+    return <StyledCard>
+        <div className="tittle">{tittle}</div>
+        {
+            amount ? 
+            <div className="ammount-wrapper"><img src={Ether} alt="" /> <span className="amount">{amount}</span> ETH
+            </div>:
+            <div><span>{content}</span> ETH</div>
+        }
+        <Button percent={percent}>{percent}</Button>
+    </StyledCard>
 }
